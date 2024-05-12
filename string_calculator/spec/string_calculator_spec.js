db9c5d5986473554fd0ab8b256e1c56e6500f6ca
=Dominik Schrenk <dsschrenk@gmail.com>
2024-05-12 18:10:52 +0200

    2nd Test green

--- string_calculator/spec/string_calculator_spec.js
+++ string_calculator/spec/string_calculator_spec.js
@@ -6,4 +6,8 @@ describe('String Calculator', function() {
 	it('should return "Spaceship lost forever." for an empty string: ""', function() {
 		expect(calculator.add('')).toEqual("Spaceship lost forever.");
 	});
+
+	it('should return "Spaceship lost forever." for input: "......"', function() {
+		expect(calculator.add('')).toEqual("Spaceship lost forever.");
+	});
 });
