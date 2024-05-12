b8a4471ef03182977f78ed4290a03c3dbcef1bc1
=Dominik Schrenk <dsschrenk@gmail.com>
2024-05-12 18:16:27 +0200

    4th test green

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -4,6 +4,10 @@ StringCalculator.prototype.add = function(map) {
 	var x = 0;
 	var y = 0;
 	
+	if (map.length === 0) {
+		return "Spaceship lost forever.";
+	} else  if (map === 'X') {
+		return '[0,0]';
+	}
 	
-	return "Spaceship lost forever.";
 };
